940bf932e1f3bbf1205446da9a656891d17e1985
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2025-12-28 13:47:29 +0000

    [LeetCode Sync] Runtime - 1 ms (24.99%), Memory - 53.2 MB (9.18%)

--- Leetcode-Submissions/0540-single-element-in-a-sorted-array/solution.java
+++ Leetcode-Submissions/0540-single-element-in-a-sorted-array/solution.java
@@ -1,24 +1,12 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n=nums.length;
-        if(n==1){
-            return nums[0];
-        }
+        int ans=0;
         for(int i=0;i<nums.length;i++){
-             if (i == 0) {
-                if (nums[i]!=(nums[i + 1]))
-                    return nums[i];
-             }
-             else if (i == n - 1) {
-                     if (nums[i]!=(nums[i - 1]))
-                        return nums[i]; 
-             } 
-            else{
-                if(nums[i]!=nums[i + 1] && nums[i]!=nums[i - 1])
-                    return nums[i];
-            }
+            ans=ans^nums[i];
         }
-        return -1;
+        // for(int i=1;i<nums.length;i++){
+        //     ans=ans^i;
+        // }
+        return ans;
     }
-    
 }
