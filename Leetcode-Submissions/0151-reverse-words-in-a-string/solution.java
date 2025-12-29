1e2e82660022f60d827a076135b7bc60f1ce3dc4
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2025-12-29 13:30:32 +0000

    [LeetCode Sync] Runtime - 8 ms (34.60%), Memory - 44.5 MB (37.19%)

--- Leetcode-Submissions/0151-reverse-words-in-a-string/solution.java
+++ Leetcode-Submissions/0151-reverse-words-in-a-string/solution.java
@@ -1,8 +1,26 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         String[] str=s.trim().split("//s+");
+//         String out="";
+//         for(int i=str.length-1;i>0;i--){
+//             out+=str[i]+" ";
+//         }
+//         return out+str[0];
+//     }
+// }
+
+
 class Solution {
     public String reverseWords(String s) {
-         s = s.trim();
-        List<String> words = Arrays.asList(s.split("\\s+"));
-        Collections.reverse(words);
-        return String.join(" ", words);
+        String[] arr = s.trim().split("\\s+");
+        int i=0,j=arr.length-1;
+        while(i<j) {
+            String t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+            i++;
+            j--;
+        }
+        return String.join(" ", arr);
     }
 }
