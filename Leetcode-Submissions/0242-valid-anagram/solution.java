19c80020109536ca5be9b7c3e5bc61c89538cd6f
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-02-08 09:12:53 +0000

    [LeetCode Sync] Runtime - 18 ms (13.43%), Memory - 46.9 MB (9.10%)

--- Leetcode-Submissions/0242-valid-anagram/solution.java
+++ Leetcode-Submissions/0242-valid-anagram/solution.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> map = new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> mp = new HashMap<>(); 
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
         }
         for(int i=0;i<t.length();i++){
-            char ch= t.charAt(i);
-            if(map.get(ch)!=null){
-                if(map.get(ch)==1){
-                    map.remove(ch);
-                }
-                else{
-                    map.put(ch,map.get(ch)-1);
-                }
-            }
-            else{
+            char ch=t.charAt(i);
+            if(!mp.containsKey(ch) || mp.get(ch) == 0){
                 return false;
             }
+            mp.put(ch,mp.get(ch)-1);
         }
-        return map.isEmpty();
+        return true;
     }
 }
