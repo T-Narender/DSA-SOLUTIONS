1e727af513e07cd6d170e07d59f828ec035bd655
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-03-04 03:55:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.6 MB (78.07%)

---
+++ Leetcode-Submissions/0213-house-robber-ii/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        return Math.max(
+            getMax(nums,0,nums.length-2),
+            getMax(nums,1,nums.length-1)
+        );
+    }
+    private int getMax(int[] nums, int start, int end){
+        int prevRob = 0, maxRob = 0;
+        for(int i=start;i<=end; i++){
+            int temp = Math.max(maxRob,prevRob+nums[i]);
+            prevRob = maxRob;
+            maxRob = temp;
+        }
+        return maxRob;
+    }
+}
