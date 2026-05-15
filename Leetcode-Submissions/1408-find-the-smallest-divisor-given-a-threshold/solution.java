240bca85d862d8a8087b7cb7e2c322a6db215901
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-05-15 17:33:16 +0000

    [LeetCode Sync] Runtime - 33 ms (6.48%), Memory - 50.7 MB (94.43%)

---
+++ Leetcode-Submissions/1408-find-the-smallest-divisor-given-a-threshold/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+          if (nums.length > threshold) return -1;
+
+        int low = 1;
+        int high = Arrays.stream(nums).max().getAsInt();
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(nums, mid) <= threshold) {
+                high = mid - 1; 
+            } else {
+                low = mid + 1; 
+            }
+        }
+
+        return low;
+    }
+    private int sumByD(int[] arr, int div) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += Math.ceil((double) num / div);
+        }
+        return sum;
+    }
+}
