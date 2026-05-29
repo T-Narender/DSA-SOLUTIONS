2b8d694fe0edd6020d99d94b400ee401374d6f14
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-05-29 06:48:02 +0000

    [LeetCode Sync] Runtime - 929 ms (90.28%), Memory - 0.0B (100.00%)

---
+++ Leetcode-Submissions/1773-percentage-of-users-attended-a-contest/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select contest_id , ROUND((COUNT(distinct user_id))*100/(select COUNT(user_id)from Users),2) as percentage
+from Register 
+GROUP BY contest_id
+ORDER BY percentage desc , contest_id
+
