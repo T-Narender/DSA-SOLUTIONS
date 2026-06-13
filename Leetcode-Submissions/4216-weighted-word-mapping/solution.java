d2af871b42ac61b126125a8f1156ce4846e4f396
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-06-13 15:13:31 +0000

    [LeetCode Sync] Runtime - 2 ms (95.78%), Memory - 46.5 MB (69.24%)

---
+++ Leetcode-Submissions/4216-weighted-word-mapping/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans = new StringBuilder();
+        for(String word:words){
+            int sum = 0;
+            for(char ch : word.toCharArray()){
+                sum+=weights[ch-'a'];
+            }
+            int rem = sum%26;
+            char mappedChar = (char)('z'-rem);
+            ans.append(mappedChar);
+        }
+        return ans.toString();
+    }
+}
