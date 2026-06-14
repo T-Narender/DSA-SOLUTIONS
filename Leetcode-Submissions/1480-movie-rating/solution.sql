eaba087498baa9930ff527a88bc4d57615538ba7
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-06-14 07:42:06 +0000

    [LeetCode Sync] Runtime - 1557 ms (38.39%), Memory - 0.0B (100.00%)

---
+++ Leetcode-Submissions/1480-movie-rating/solution.sql
@@ -0,0 +1,16 @@
+(select name as results
+from Users
+inner join MovieRating USING (user_id)
+group by user_id
+order by count(rating) desc, name
+limit 1)
+
+union all
+
+(select title as results
+from Movies
+inner join MovieRating USING (movie_id)
+where MONTH(created_at) = '02' and YEAR(created_at) = '2020'
+group by title
+order by avg(rating) desc , title
+limit 1)
