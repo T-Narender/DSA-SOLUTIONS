3dcfba0149d3320cf3b1519ce4cb39c16051e346
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-06-20 06:59:25 +0000

    [LeetCode Sync] Runtime - 698 ms (91.78%), Memory - 0.0B (100.00%)

---
+++ Leetcode-Submissions/1462-list-the-products-ordered-in-a-period/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select product_name, sum(o.unit) as unit
+from Products p
+inner join Orders o
+on p.product_id = o.product_id
+where
+o.order_date >= '2020-02-01' AND o.order_date < '2020-03-01'
+group by p.product_id,p.product_name
+having sum(o.unit) >=100
+
