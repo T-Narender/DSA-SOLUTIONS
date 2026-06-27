35bc183f8f564fbb55c7276a171b54ac5021e3c4
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-06-27 06:28:12 +0000

    [LeetCode Sync] Runtime - 1 ms (86.51%), Memory - 44.3 MB (39.04%)

--- Leetcode-Submissions/0078-subsets/solution.java
+++ Leetcode-Submissions/0078-subsets/solution.java
@@ -1,16 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>>outer=new ArrayList<>();
-        outer.add(new ArrayList<>());
-        for(int num: nums){
-            int n=outer.size();
-            for(int i=0;i<n;i++){
-                List<Integer>internal=new ArrayList<>(outer.get(i));
-                internal.add(num);
-                outer.add(internal);
-            }
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0,nums,new ArrayList<>(),res);
+        return res;
+    }
+
+    private void backtrack(int idx,int[] nums,List<Integer> curr,List<List<Integer>> res){
+        //base case
+        if(idx==nums.length){
+            res.add(new ArrayList<>(curr));
+            return;
         }
-        return outer;
-        
+        //choice 1 include-
+        curr.add(nums[idx]);
+        backtrack(idx+1,nums,curr,res);
+
+        //choice 2 exclude-
+        curr.remove(curr.size()-1);
+        backtrack(idx+1,nums,curr,res);
     }
 }
