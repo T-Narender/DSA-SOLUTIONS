8d066b9af84109a7baf0941cf53fb816ebd323cb
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-06-27 09:11:22 +0000

    [LeetCode Sync] Runtime - 6 ms (72.13%), Memory - 51 MB (5.77%)

---
+++ Leetcode-Submissions/0040-combination-sum-ii/solution.java
@@ -0,0 +1,27 @@
+// class Solution {
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
+//     }
+// }
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        findComb(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+    private void findComb(int start,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
+        if(target==0){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=start;i<arr.length;i++){
+           if(i>start && arr[i]==arr[i-1]) continue;
+           if(arr[i]>target) break;
+           ds.add(arr[i]);
+           findComb(i+1,arr,target-arr[i],ans,ds);
+           ds.remove(ds.size()-1);
+        }
+    }
+}
