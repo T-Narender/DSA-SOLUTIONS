ecc52086fd279b5ba02dd5448b8f9db540bf54c4
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-06-29 15:15:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (11.40%)

--- Leetcode-Submissions/0033-search-in-rotated-sorted-array/solution.java
+++ Leetcode-Submissions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,32 +1,10 @@
 class Solution {
-   public int search(int[] arr, int target) {
-    int low = 0, high = arr.length - 1;
-
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-
-        if (arr[mid] == target)
-            return mid;
-
-        // Check if left half is sorted
-        if (arr[low] <= arr[mid]) {
-            if (arr[low] <= target && target < arr[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
             }
         }
-        // Right half is sorted
-        else {
-            if (arr[mid] < target && target <= arr[high]) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
+        return -1;
     }
-
-    return -1;
-}
-
 }
