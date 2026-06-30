c944d6d358c1c451bc04d519446c797903673763
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-06-30 13:35:14 +0000

    [LeetCode Sync] Runtime - 12 ms (75.50%), Memory - 46.4 MB (42.16%)

---
+++ Leetcode-Submissions/1460-number-of-substrings-containing-all-three-characters/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int []lastSeen = new int[3];
+        Arrays.fill(lastSeen,-1);
+        int total=0;
+
+        for(int i=0;i<s.length();i++){
+            lastSeen[s.charAt(i)-'a']=i;
+            if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
+                int minIdx = Math.min(lastSeen[0],Math.min(lastSeen[1],lastSeen[2]));
+                total+=minIdx+1;
+            }
+        }
+        return total;
+    }
+}
