7a0fd27f5323710512880dc8237cb6ed8845619e
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-07-12 05:53:43 +0000

    [LeetCode Sync] Runtime - 34 ms (31.86%), Memory - 77.2 MB (17.88%)

---
+++ Leetcode-Submissions/1256-rank-transform-of-an-array/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int[] temp = arr.clone();
+        Arrays.sort(temp);
+        int rank=1;
+        for(int num:temp){
+            if(!hm.containsKey(num)){
+                hm.put(num,rank++);
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=hm.get(arr[i]);
+        }
+        return arr;
+    }
+}
