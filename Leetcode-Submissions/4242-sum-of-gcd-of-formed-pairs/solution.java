6934a3b995b5c3e2cff22c629a7bdf93df1c2a59
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-07-16 14:31:14 +0000

    [LeetCode Sync] Runtime - 58 ms (36.82%), Memory - 107.9 MB (83.16%)

---
+++ Leetcode-Submissions/4242-sum-of-gcd-of-formed-pairs/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long gcdSum(int[] nums) {
+        int n=nums.length;
+        int max=-1;
+        int prefixGcd[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            max=Math.max(nums[i],max);
+            prefixGcd[i]=gcd(nums[i],max);
+        } 
+        long ans=0;
+        Arrays.sort(prefixGcd);
+
+        for(int i=0,j=n-1;i<j;i++,j--){
+            ans+=gcd(prefixGcd[i],prefixGcd[j]);
+        }
+        return ans;
+    }
+    int gcd(int a , int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+}
