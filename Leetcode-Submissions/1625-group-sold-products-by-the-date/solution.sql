0a46894f92585e48bbb1ad9f810338e6f1666f03
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-08-03 07:08:16 +0000

    [LeetCode Sync] Runtime - 567 ms (22.07%), Memory - 0.0B (100.00%)

--- Leetcode-Submissions/1625-group-sold-products-by-the-date/solution.sql
+++ Leetcode-Submissions/1625-group-sold-products-by-the-date/solution.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
-select sell_date, count(distinct product) as num_sold, 
-    GROUP_CONCAT(
-        distinct product
-        order by product
-        separator ','
-    ) as products
-from Activities
-group by sell_date
-order by sell_date , product
+select sell_date,
+    count(distinct product) as num_sold,
+    group_concat(distinct product order by product) as products
+    From Activities
+    group by sell_date
+    order by sell_date
