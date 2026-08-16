42224becf900512774a0636444e1ebd8c9acb55e
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-08-16 09:29:06 +0000

    [LeetCode Sync] Runtime - 5 ms (80.59%), Memory - 46.5 MB (90.79%)

---
+++ Leetcode-Submissions/0210-course-schedule-ii/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        int[] inDegree = new int[numCourses];
+
+        for(int[]pre:prerequisites){
+            int course = pre[0];
+            int prerequisite = pre[1];
+            adj.get(prerequisite).add(course);
+            inDegree[course]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0){
+                queue.offer(i);
+            }
+        }
+        int[] order = new int[numCourses];
+        int index = 0; 
+
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            order[index++] = current;
+            for(int next : adj.get(current)){
+                inDegree[next]--;
+
+                if(inDegree[next]==0){
+                    queue.offer(next);
+                }
+            }
+        }
+        return (index == numCourses) ? order : new int[0];
+    
+    }
+}
