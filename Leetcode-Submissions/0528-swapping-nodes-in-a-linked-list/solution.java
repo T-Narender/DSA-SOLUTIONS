016be378b0f9f309057a023aba2d677b358a576d
Tanguturi Narender <158013436+T-Narender@users.noreply.github.com>
2026-08-28 15:07:08 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 118.2 MB (97.01%)

---
+++ Leetcode-Submissions/0528-swapping-nodes-in-a-linked-list/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        for(int i=1;i<k;i++){
+            fast = fast.next;
+        }
+        ListNode target1 = fast;
+
+        while(fast.next!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        ListNode target2 = slow;
+
+        int temp = target1.val;
+        target1.val = target2.val;
+        target2.val = temp;
+
+        return head;
+    }
+    
+}
